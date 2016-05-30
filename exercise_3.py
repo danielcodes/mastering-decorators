05dfaee7a7f5a66615a0c7fc1e739f15567acb0c
Daniel Chia <daniel_1118@hotmail.com>
2016-05-30 15:51:19 -0400

    used two list to cache keys and values, still need to create queue like functionality

--- exercise_3.py
+++ exercise_3.py
@@ -1,31 +1,48 @@
 # Write a decorator that caches the n most recent return values from the function it is applied to, so that the function does not have to be invoked again.
-# so an outer wrap, to pass the n parameter
-# what it sounds like to me, create decorator that caches n values
-# but how to make a dictionary of certain length?
-
-# this is for later, but the actual problem actually involves creating a queue like data structure, to save the n most recent
-
-# ok so the n values cache is doable, start there
 
 # create a dictionary, 
 # start by creating a queue like structure
 
+# CURRENT PROBLEM
+# the actual data structure that is needed is a queue of n length
+# if the queue is full, push an element in and drop the first one
+
+# another problem is the cache, since we have a queue (list), we have to store a dictionary for each element
+# on lookup, we have to collect the keys and do a check against that
+
 # its already a mess
+
+from functools import reduce
+
 def saveresults(n):
     def decorator(fn):
 
         # todo: set a length 
-
-        cache = {}
+        cache = []
+        keys = []
 
         def inner(*args, **kwargs):
-            if args[0] not in cache:
-                print('caching now')
-                cache[args[0]] = fn(*args, **kwargs)
 
-            print('the arg is ', args[0])
-            print('the current cache is ', cache)
-            return cache[args[0]]
+            # two checks, the key is not there and list is not full
+            if args[0] not in keys:
+                print('caching now***********')
+
+                # push a dictionary in
+                cache.append(fn(*args, **kwargs))
+                keys.append(args[0])
+
+                print('the arg is ', args[0])
+                print('the current keys are ', keys, ' *********')
+                print('the current cache is ', cache, ' *********')
+
+                # the return is also a pain
+
+            # have to do a look up here as well
+            # if the value is in the cache, thennnn
+            print('the index of the key is ', keys.index(args[0]))
+            index = keys.index(args[0])
+
+            return cache[index]
         return inner    
     return decorator
 
@@ -33,9 +50,11 @@ def saveresults(n):
 def powerup(a):
     return a * a
 
-print('the real result is ', powerup(5))
-print('the real result is ', powerup(10))
-print('the real result is ', powerup(5))
+a = [powerup(i) for i in range(6)]
+
+print('the a list is ', a)
 
 
+b = [powerup(i) for i in range(3)]
 
+print('the b list is ', b)
